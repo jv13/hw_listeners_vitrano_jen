0fefe1c28753b94c8d3fa0d7193767181c997355
Jen Vitrano <vitranojen@gmail.com>
2016-03-16 23:39:51 -0400

    Add id to heading and event click
    Added an id to the h1, as well as added an event click in JS so that when the h1 is clicked the console logs the message "This click is number".

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var heading = document.getElementById( 'mainHeader' ) ;
+heading.addEventListener( 'click', function() {
+
+	  // function clickHeading ( number ) {
+		//   var clickNumber = 0;
+		//
+		// 	while (number > 0) {
+		// 		clickNumber += 1;
+		//
+		// 		}
+		// }
+		// var myElement.innerHTML= "<p>This is click number</p>";
+		console.log( 'This click is number' ) ;
+
+} );
