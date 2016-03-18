8de2fc11494396dde0e8c7e0cb378332f5c2157c
Jen Vitrano <vitranojen@gmail.com>
2016-03-18 00:01:46 -0400

    Update button event listener comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var img = document.getElementById( 'funfact' );
 var info = document.getElementById( 'funfact' );
 
-//Listen for events on the button, when the button is clicked create a paragraph and add the hidden text and image.
+//Listen for events on the button
+//When the button is clicked create a paragraph and image and adds the hidden text and image.
 img.addEventListener( 'click', function( event ) {
 	var info = document.createElement( 'paragraph' );
   info.innerHTML = '<p>Fun Fact: I worked in Magic Kingdom at the Emporium from August 2011 - May 2012</p>';
