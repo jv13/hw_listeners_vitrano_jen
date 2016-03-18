2b92f6c0e028c6f978d29cec9faebb4660a5e5f2
Jen Vitrano <vitranojen@gmail.com>
2016-03-18 11:51:01 -0400

    Update comments in js for better user readability

--- js/main.js
+++ js/main.js
@@ -4,26 +4,30 @@ var heading = document.getElementById( 'mk' );
 var i = 0;
 
 //Listen for events on the button
-//When the button is clicked create a paragraph and image and adds the hidden text and image.
 img.addEventListener( 'click', function( event ) {
+
+	//When the button is clicked a new paragraph is created with fun fact text tha becomes visible.
 	var info = document.createElement( 'paragraph' );
   info.innerHTML = '<p>Fun Fact: I worked in Magic Kingdom at the Emporium from August 2011 - May 2012!</p>';
 	document.getElementById( 'container' ).appendChild( info );
 
+	//When the button is clicked a new image is created that becomes visible.
 	var img = document.createElement( 'img' );
   img.setAttribute( 'src', 'img/night.jpg', 'fun' );
 	document.getElementById( 'container' ).appendChild( img );
 } );
 
 //Listen for event on the h2
-//When the user places their mous over the h2, new text displays after the html in the h2.
 heading.addEventListener( 'mouseover', function() {
+
+	  //When the user places their mous over the h2, new text displays after the html in the h2.
 		document.getElementById( 'mk' ).innerHTML = 'Magic Kingdom is my favorite park!';
 	} );
 
 //Listen for event on the resize of the screen
-//When the user resizes the screen a paragraph appears after the content and lets the user know how many times they have resized the screen.
 window.addEventListener( 'resize', function() {
+
+	//When the user resizes the screen a paragraph appears after the content and lets the user know how many times they have resized the screen.
 	var resize = i += 1;
 	document.getElementById( 'times' ).innerHTML = 'This window was resized ' + resize + ' times!';
 } );
