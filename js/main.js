622286d9f3ff5ddfd2baa4e8e6edb93b40b0c72f
Jen Vitrano <vitranojen@gmail.com>
2016-03-18 13:17:44 -0400

    Update event listener
    Updated event listener so that the new paragraph text and the click number were on the same line

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
 var i = 0;
 var heading = document.getElementById( 'mainHeader' );
 
-heading.addEventListener( 'click', function( event ) {
-	var myElement = document.createElement( 'paragraph' );
-	var count = i += 1;
-	myElement.innerHTML = '<p>This is click number </p>' + count;
-	document.getElementById( 'goesup' ).appendChild( myElement );
-} );
-
-// document.getElementById( 'mainHeader' ).addEventListener( 'click', function( event ) {
-//
-//     // display the current click count inside the clicked div
-// 		var myElement = document.createElement( 'paragraph' );
-// 		document.getElementById( 'goesup' ).appendChild( myElement );
-//     event.target.innerHTML = 'This is click number ' + event.detail;
-//   }, false );
+heading.addEventListener( 'click', function() {
+		var myElement = document.createElement( 'paragraph' );
+		var count = i += 1;
+		myElement.innerHTML = '<p>This is click number ' + count;
+		document.getElementById( 'container' ).appendChild( myElement );
+  } );
